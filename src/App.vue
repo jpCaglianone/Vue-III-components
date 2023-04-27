5ce884d2c5c2e6523ea4f05eef0b7ce10fd77071
jpCaglianone <89998145+jpCaglianone@users.noreply.github.com>
2023-04-27 22:23:36 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+
+  <botao msg="teste"></botao>
+
+  
+</template>
+
+<script>
+
+
+import myButton from "./components/myComp.vue";
+
+
+export default {
+  name: "App",
+  components: {
+    botao: myButton
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/myComp.vue
@@ -0,0 +1,25 @@
+
+
+<script>
+
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+  
+</script>
+
+<template>
+    <button> {{msg}} </button>
+</template>
+
+<style scoped>
+button{
+  border-radius: 5px;
+  color:white;
+  background:green;
+}
+</style>
